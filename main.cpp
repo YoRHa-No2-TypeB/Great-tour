3b8aae2d3cc9142a182c1cc10cffa411a422c8c3
YoRHa-No2-TypeB <64475340+YoRHa-No2-TypeB@users.noreply.github.com>
2020-09-20 19:14:59 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,169 +1,52 @@
-#include <iostream>
+a = set()
+b = set()
 
-using namespace std;
+print("Введите размерность множества A")
+n = input()
+n = int(n)
+print("Введите размерность множества B")
+m = input()
+m = int(m)
 
-int main()
-{
-    int *a;
-    int *b;
-    int n,m,c;
+i = 0
+print("Введите элементы множества A")
+while i <n :
+    number = input()
+    a.add(number)
+    i = i + 1 
 
-    cout << "n = ";
-    cin >> n;
+i = 0
+print("Введите элементы множества B")
+while i <m :
+    number = input()
+    b.add(number)
+    i = i + 1 
 
-    cout << "m = ";
-    cin >> m;
+output = a.union(b)
+diff_a = a.difference(b)
+symm_diff = a.symmetric_difference(b)
+intersect = a.intersection(b)
+iss = a.issubset(b)
 
-    a= new int[n];
-    b= new int[m];
+k = 0
+while k<6 :
+    print("Выберите операцию")
+    print("1 - объединение")
+    print("2 - пересечение")
+    print("3 - разность")
+    print("4 - симметрическая разность")
+    print("5 - принадлежность А к Б")
+    print("Введите другое число для выхода")
+    k = input()
+    if k == "1" :
+        print(output)
+    elif k == "2" :
+        print(intersect) 
+    elif k == "3" :
+        print(diff_a) 
+    elif k == "4" :
+        print(symm_diff) 
+    elif k == "5" :
+        print(iss)
+    k = int(k)
 
-
-
-    for (int i = 0; i < n; i++)
-        {
-        cout << "a[" << i << "] = ";
-        cin >> a[i];
-        }
-
-    for (int i = 0; i < m; i++)
-        {
-        cout << "b[" << i << "] = ";
-        cin >> b[i];
-        }
-
-    int temp;
-
-    for (int i = 0; i < n - 1; i++)
-        {
-        for (int j = 0; j < n - i - 1; j++)
-        {
-            if (a[j] > a[j + 1])
-            {
-                temp = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = temp;
-            }
-        }
-    }
-
-for (int i = 0; i < m - 1; i++)
-        {
-        for (int j = 0; j < m - i - 1; j++)
-        {
-            if (b[j] > b[j + 1])
-            {
-                temp = b[j];
-                b[j] = b[j + 1];
-                b[j + 1] = temp;
-            }
-        }
-    }
-
-    for (int i = 0; i < n; i++)
-        {
-        cout << a[i] << " ";
-        }
-    cout << endl;
-
-      for (int i = 0; i < m; i++)
-        {
-        cout << b[i] << " ";
-        }
-    cout << endl;
-
-    for (int i = 1; i < n-1; i++)
-        {
-        if ((a[i]>a[i-1]) && (a[i]<a[i+1]))
-            a[i]=a[i];
-        else a[i]=0;
-        }
-
-       for (int i = 0; i < n - 1; i++)
-        {
-        for (int j = 0; j < n - i - 1; j++)
-        {
-            if (a[j] > a[j + 1])
-            {
-                temp = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = temp;
-            }
-        }
-    }
-
-    for (int i = 0; i < n; i++)
-        {
-        if (a[i]!=0)
-        cout << a[i] << " ";
-        }
-    cout << endl;
-
-    for (int i = 1; i < m-1; i++)
-        {
-        if ((b[i]>b[i-1]) && (b[i]<b[i+1]))
-            b[i]=b[i];
-        else b[i]=0;
-        }
-
-       for (int i = 0; i < m - 1; i++)
-        {
-        for (int j = 0; j < m - i - 1; j++)
-        {
-            if (b[j] > b[j + 1])
-            {
-                temp = b[j];
-                b[j] = b[j + 1];
-                b[j + 1] = temp;
-            }
-        }
-    }
-
-    for (int i = 0; i < m; i++)
-        {
-        if (b[i]!=0)
-        cout << b[i] << " ";
-        }
-    cout << endl;
-
-
-    cout << "Vv No operacii";
-    cout << endl;
-    cout << "1-obedinenie";
-    cout << endl;
-    cout << "2-peresechenie";
-    cout << endl;
-    cout << "3-raznost'";
-    cout << endl;
-    cout << "4-simmetrichnaya raznost'";
-    cout << endl;
-    cout << "5-obratnoe dopolnenie";
-    cout << endl;
-    cout << "6-dopolnenie ";
-    cout << endl;
-    cin >> c;
-
-
-
-     //if (c=1)
-     //{
-      // for (int i = 0; i < n; i++)
-
-
-     //}
-
- if (c=2)
-     {
-       for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-        if ((a[i]!=0)&&(b[j]!=0))
-        if (a[i]==b[j])
-            cout<<a[i]<<" ";
-     }
-
-
-
-    delete [] a;
-    delete [] b;
-
-    return 0;
-}
